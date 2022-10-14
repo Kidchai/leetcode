d307c16c59d30b46fa83ac3c70095c96abca1304
Kidchai <o.naibova@gmail.com>
2022-10-14 13:46:14 -0500

    Add Dependency Injection

--- src/main/java/kidchai/calories/counter/SpringMain.java
+++ src/main/java/kidchai/calories/counter/SpringMain.java
@@ -1,6 +1,9 @@
 package kidchai.calories.counter;
 
+import kidchai.calories.counter.model.Role;
+import kidchai.calories.counter.model.User;
 import kidchai.calories.counter.repository.UserRepository;
+import kidchai.calories.counter.service.UserService;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -14,6 +17,10 @@ public class SpringMain {
 //        UserRepository userRepository = (UserRepository) appCtx.getBean("inmemoryUserRepository");
         UserRepository userRepository = appCtx.getBean(UserRepository.class);
         userRepository.getAll();
+
+        UserService userService = appCtx.getBean(UserService.class);
+        userService.create(new User(null, "userName", "email@mail.ru", "password", Role.ADMIN));
+
         appCtx.close();
     }
 }
